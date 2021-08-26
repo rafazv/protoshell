3ddc1eb39c8b3b60ab1beb04bcae5187ba06604f
Rafaela <ral@icomp.ufam.edu.br>
2021-08-26 14:47:32 -0400

    Fix bugs

--- protoshell.c
+++ protoshell.c
@@ -9,9 +9,6 @@
 #define MAX_ARG 10 // número máximo de argumentos
 #define INPUT_DEL " \n" // caracteres a serem deletados pelo strtok
 
-// limpando o terminal usando sequências de escape
-#define clear() printf("\033[H\033[J")
-
 // declaração antecipada das funções usadas no programa
 void iniciar_terminal();
 void leitura_input(char* usu_input);
@@ -20,7 +17,7 @@ int parse_input(char* usu_input, char** parsed_input);
 void pwd();
 int cd(char** args);
 int help();
-int exec_prog(char** args);
+int exec_prog(char** args, int tam);
 int exec_prog_piped (char** args1, char** args2);
 
 int main() {
@@ -29,6 +26,7 @@ int main() {
     char **usu_input_pipe; // input do usuario passado para ser divido - pipe
     char **parsed_input; // input depois do parse
     char **parsed_input2; // input depois do parse - comando 2
+    int tam;
 	
     usu_input = malloc(INPUT_SIZE * sizeof(char));
 	
@@ -54,7 +52,7 @@ int main() {
         printf("> ");
         leitura_input(usu_input); // leitura input
         parse_input_pipe(usu_input, usu_input_pipe); // parse do input caso pipe
-        parse_input(usu_input_pipe[0], parsed_input); // parse do input - tirando "\n" e espaço
+        tam = parse_input(usu_input_pipe[0], parsed_input); // parse do input - tirando "\n" e espaço
 
         // verificar se exit
         if (parsed_input[0] != NULL && (!strcmp(parsed_input[0], "exit"))) {	
@@ -72,9 +70,9 @@ int main() {
             // executa o programa que usa pipe
 			exec_prog_piped(parsed_input, parsed_input2);
 		}
-
-        // exec comando único
-        else if (parsed_input[0] != NULL) exec_prog(parsed_input);
+        else if (parsed_input[0] != NULL) { // exec comando único
+            exec_prog(parsed_input, tam);
+        }
     }
 
     return 0;
@@ -82,15 +80,12 @@ int main() {
 
 // boas vindas do terminal
 void iniciar_terminal() {
-    clear();
     printf("******************"
         "************************");
     printf("\n\n\n\t****PROTO TERMINAL****");
     printf("\n\n\n\n*******************"
         "***********************");
-    printf("\n");
-    sleep(1);
-    clear();
+    printf("\n\n");
     printf("Digite 'help' para listar os comandos disponiveis\n");
     printf("\n");
 }
@@ -98,7 +93,6 @@ void iniciar_terminal() {
 // leitura input
 void leitura_input(char* usu_input) {
 	fgets(usu_input, INPUT_SIZE, stdin);
-	// return 1;
 }
 
 // input: pipe1 | pipe2
@@ -122,16 +116,17 @@ int parse_input_pipe(char* usu_input, char** usu_input_pipe) {
 
 // divide o input e armazena em parsed_input
 int parse_input(char* usu_input, char** parsed_input) {
-	int i = 0;
+	int i = 0, j = 0;
 	char* buf;
 	
 	buf = strtok(usu_input, INPUT_DEL);
 	while (buf != NULL) {
 		parsed_input[i++] = buf;
+        j++;
 		buf = strtok(NULL, INPUT_DEL);
 	}
 	while (i < MAX_ARG) parsed_input[i++] = NULL;
-	return 1;
+	return j;
 }
 
 // pwd - diretorio corrente
@@ -175,10 +170,10 @@ int help() {
 }
 
 // executa prog caso não haja pipe
-int exec_prog(char** args) {
+int exec_prog(char** args, int tam) {
     int p_pid = -1;
 	int exec_resp;
-	int in, out;
+	int in, out, i;
 
     if (!strcmp(args[0], "cd")) return cd(args);
     if (!strcmp(args[0], "help")) return help();
@@ -189,28 +184,37 @@ int exec_prog(char** args) {
         printf("\n### ERRO: Fork falhou. Abortando...\n\n");
         exit(1);
     } else if (p_pid == 0) {
-        
-        if (args[1] != NULL && !strcmp(args[1], "<")) {
-            in = open(args[2], O_RDONLY);
-            if (in < 0) {
-                printf("\n### ERRO: arquivo nao encontrado\n\n");
-                close(in);
-                exit(1);
-            }
-            dup2(in, 0);
-            close(in);
-        } 
 
-        if (args[1] != NULL && !strcmp(args[1], ">")) {
-            out = open(args[2], O_WRONLY | O_CREAT, 0666);
-            if (out < 0) {
-                printf("\n### ERRO: nao foi possivel criar o arquivo de saida\n\n");
-                close(out);
-                exit(1);
+        for (i=0; i<tam; i++) {
+            if (args[i] != NULL) {
+                if (!strcmp(args[i], "<")) {
+                    in = open(args[i+1], O_RDONLY, 0);
+                    if (in < 0) {
+                        printf("\n### ERRO: arquivo nao encontrado\n\n");
+                        close(in);
+                        exit(1);
+                    }
+                    dup2(in, 0);
+                    close(in);
+                } 
+
+                if (!strcmp(args[i], ">")) {
+                    out = open(args[i+1], O_WRONLY | O_CREAT | O_TRUNC, 0600);
+                    if (out < 0) {
+                        printf("\n### ERRO: nao foi possivel criar o arquivo de saida\n\n");
+                        close(out);
+                        exit(1);
+                    }
+                    dup2(out, 1);
+                    close(out);
+                }
+
+                // if (!strcmp(args[i], "&")) {
+
+                // }
             }
-            dup2(out, 1);
-            close(out);
-        } 
+        }
+
 
         // executa programas
         exec_resp = execvp(args[0], args);
